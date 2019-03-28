5561a071f6a0af30c969df166bd8e89604ba2718
skobraf <skobraf@gmail.com>
2019-03-28 13:00:13 +0000

    first commit

---
+++ page.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="eng">
+	<head>
+		<meta charset="utf-8">
+		<title>first page</title>
+	</head>
+	<body>
+		<h1>this is HTML</h1>
+		<?php echo "this is PHP"; ?>
+	</body>
+</html>
