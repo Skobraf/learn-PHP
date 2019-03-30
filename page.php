1678318365be195a85e853de8aee8f55218c2914
skobraf <skobraf@gmail.com>
2019-03-30 16:21:41 +0000

    Exercice 4 TP 2

--- page.php
+++ page.php
@@ -6,14 +6,29 @@
 	</head>
 	<body>
 		<?php 
-			$tab= [];
-			for ($i=0; $i < 10; $i++) { 
-				$tab["User".$i] = $i;
-			}
-			foreach ($tab as $key => $value) {
-				echo "$key ... $value";
-			}
+			$tab = array("Ayoub"=>array("abed", "ahfir"), "Khalid"=>array("chigan", "rachidia"));
 		 ?>
+		 <ul>
+		 	<?php 
+		 		$personnes = array(
+					"nom1"=>array("prenom 1"=>"Ali","ville 1"=>"Rabat","age 1"=>"35"),
+					"nom2"=>array("prenom 2"=>"Ahmed","ville 2"=>"Casa","age 2"=>"22"),
+					"nom3"=>array("prenom 3"=>"Mourad","ville 3"=>"Kenitra","age 3"=>"47"));
+
+					echo "<table border=\"1\">";
+					foreach($personnes as $cle=>$tab)
+					{
+					echo "<tr>";
+					echo '<th>'.$cle.'</th>';
+					 foreach($tab as $key=>$valeur)
+					 {
+					 echo "<td> $key : <b> $valeur </b></br></td>";
+					 }
+					 echo "</tr>";
+					}
+					echo "</table>";
+		 	 ?>
+		 </ul>
 		 </ul>
 	</body>
 </html>
