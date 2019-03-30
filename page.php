67ee884ffc5d4f3674ac412037c4282e93eaceb0
skobraf <skobraf@gmail.com>
2019-03-30 16:57:52 +0000

    exercice 4 TP 3

--- page.php
+++ page.php
@@ -5,30 +5,28 @@
 		<title>l'instruction include</title>
 	</head>
 	<body>
+		<form name="tri" method="post" action="trier.php">
+			<input type="text" name="clt1"/> <br/>
+			<input type="text" name="clt2"/><br/>
+			<input type="text" name="clt3"/><br/>
+			<input type="submit" name="valider" value="Trier"/>
+		</form>
 		<?php 
-			$tab = array("Ayoub"=>array("abed", "ahfir"), "Khalid"=>array("chigan", "rachidia"));
+			if(isset($_POST['valider'])){
+				$clt1=$_POST['clt1'];
+				$clt2=$_POST['clt2'];
+				$clt3=$_POST['clt3'];
+				$tab=array('client1'=>$clt1,'client2'=>$clt2,'client3'=>$clt3);
+				$tablow = array_map('strtolower', $tab);
+				array_multisort($tablow, SORT_ASC, SORT_STRING);
+				echo "<table border=\"1\">";
+				foreach($tablow as $cle => $valeur){
+				echo "<tr>";
+				echo '<th>'.$cle.'</th>';
+				echo '<td>'.$valeur. '</br></td>';
+				echo "</tr>";}
+				echo "</table>";
+				}
 		 ?>
-		 <ul>
-		 	<?php 
-		 		$personnes = array(
-					"nom1"=>array("prenom 1"=>"Ali","ville 1"=>"Rabat","age 1"=>"35"),
-					"nom2"=>array("prenom 2"=>"Ahmed","ville 2"=>"Casa","age 2"=>"22"),
-					"nom3"=>array("prenom 3"=>"Mourad","ville 3"=>"Kenitra","age 3"=>"47"));
-
-					echo "<table border=\"1\">";
-					foreach($personnes as $cle=>$tab)
-					{
-					echo "<tr>";
-					echo '<th>'.$cle.'</th>';
-					 foreach($tab as $key=>$valeur)
-					 {
-					 echo "<td> $key : <b> $valeur </b></br></td>";
-					 }
-					 echo "</tr>";
-					}
-					echo "</table>";
-		 	 ?>
-		 </ul>
-		 </ul>
 	</body>
 </html>
