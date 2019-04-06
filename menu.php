177eba4cb3a76bd994ea8c616f47f2e128939dcb
skobraf <skobraf@gmail.com>
2019-04-06 18:32:13 +0100

    inserting values into tables

--- menu.php
+++ menu.php
@@ -11,14 +11,11 @@
 			try{
 				$connextion = new PDO("mysql:host=$serveur;dbname=test",$login,$pass);
 				$connextion->setAttribute(PDO::AFTER_ERRMODE, PDO::ERRMODE_EXCEPTION);
-				$codesql = "CREATE TABLE User(
-					id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-					nom VARCHAR(50),
-					prenom VARCHAR(50),
-					email VARCHAR(70)
-					)";
-					$connexion->exec($codesql);
-				echo 'Created!';
+				$insrtion = "INSERT INTO User(nom,prenom,email)
+							VALUES('Abed','Ayoub', 'skobraf@gmail.com')";
+
+					$connexion->exec($insrtion);
+				echo 'Inserted!';
 			}
 			catch(PDOException $e){
 				echo "Echec:" .$e->getMessage();
