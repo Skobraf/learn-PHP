37c561921e2a6ebb104505a6e6838a97ed2e716b
skobraf <skobraf@gmail.com>
2019-05-01 21:55:53 +0100

    sort result array

--- page.php
+++ page.php
@@ -11,6 +11,21 @@
 			<input type="text" name="input3">
 			<input type="text" name="trier" value="trier">
 		</form>
-		
+		<?php 
+			if(isset($_POST['trier'])) {
+				$input1 = $_POST['input1'];
+				$input2 = $_POST['input2'];
+				$input3 = $_POST['input3'];
+				$tab = array('client1'=>$input1, 'client2'=>$input2, 'client3'=>$input3);
+				$array_lowercase = array_map('strlower', $tab);
+				array_multisort($array_lowercase, SORT_ASC, SORT_STRING, $tab);
+				echo "<table border=\"1\">";
+				foreach($tab as $cle => $value) {
+					echo "<tr><th>".$cle."</th>";
+					echo "<td>".$value."</br></td><tr>";
+				}
+				echo "</table>";
+			}
+		 ?>
 	</body>
 </html>
