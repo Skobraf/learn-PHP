5111eb623de5029230f82bddd4ad7f9f431f317f
skobraf <skobraf@gmail.com>
2019-05-01 21:43:20 +0100

    setup form

--- page.php
+++ page.php
@@ -5,9 +5,11 @@
 		<title>l'instruction include</title>
 	</head>
 	<body>
-		<form method="post" action="<?php echo htmlspecialchars("menu.php");?>">
-			<label for="name">Name</label>
-			<input type="text" name="name" required="please enter name">
+		<form action="page.php" method="POST">
+			<input type="text" name="input1">
+			<input type="text" name="input2">
+			<input type="text" name="input3">
+			<input type="text" name="trier" value="trier">
 		</form>
 		
 	</body>
