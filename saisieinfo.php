d1ac78a2c539dbc8cb2fb229947f62223f4ec63b
skobraf <skobraf@gmail.com>
2019-05-17 00:45:25 +0100

    added form info

---
+++ saisieinfo.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<title>SasieInfo</title>
+	</head>
+	<body>
+		
+		<form action="cookie_access.php">
+			<fieldset>
+				<legend>voitre nom et mot d pass</legend>
+				<label for="nom"> Nom</label>
+				<input type="text name="nom">
+				<label for="password"> Password</label>
+				<input type="text" name="password">
+				<br />
+				<input type="submit" value="Envoyer">
+				<input type="reset" value="effecer">
+			</fieldset>
+		</form>
+	</body>
+</html>
