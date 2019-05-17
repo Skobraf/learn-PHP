9a53f5525e795ccbdde80cda380bde3f4dab4434
skobraf <skobraf@gmail.com>
2019-05-17 01:04:53 +0100

    form for session

---
+++ saisieinfo2.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>Accès réservé au site</title>
+</head>
+<body>
+<form method="post" action="accessession.php">
+<fieldset>
+<legend>Saisissez votre nom et mot de passe</legend>
+<label>Nom :
+<input type="text" name="nom" />
+</label><br /><br />
+<label>Password :
+<input type="text" name="pass" />
+</label><br />
+<input type="submit" value="Envoyer" />
+<input type="reset" value="Effacer" />
+</fieldset>
+</form>
+</body>
+</html>
